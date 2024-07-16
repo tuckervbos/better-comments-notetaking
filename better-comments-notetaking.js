b92e700e49516fab8b74d9be921b2fb993db755d
Tucker VandenBos <tuckervbos@gmail.com>
2024-07-16 11:22:31 -0500

    initial commit

---
+++ better-comments-notetaking.js
@@ -0,0 +1,117 @@
+/*
+
+! -------------------------------- BETTER COMMENTS --------------------------------
+
+! red
+===> coral
+==> orange
+=> yellow
+> lime
+green (normal comment)
+= cyan
+- midnight
+* blurple
+
+! HOW TO USE !
+* install BETTER COMMENTS extension 
+* go to settings.json file
+    >  under "better-comments.tags" [ ... ]
+        * delete contents *
+        * paste contents below *
+        * or tinker with the settings below => 
+
+"better-comments.tags": [
+
+    {
+        "tag": "!",
+        "color": "#FF2D00",
+        "strikethrough": false,
+        "underline": false,
+        "backgroundColor": "transparent",
+        "bold": false,
+        "italic": false
+    },
+    
+    {
+        "tag": "===>",
+        "color": "#ff7f50",
+        "strikethrough": false,
+        "underline": false,
+        "backgroundColor": "transparent",
+        "bold": false,
+        "italic": false
+    },
+
+    {
+        "tag": "==>",
+        "color": "#FFA500",
+        "strikethrough": false,
+        "underline": false,
+        "backgroundColor": "transparent",
+        "bold": false,
+        "italic": false
+    },
+
+    {
+        "tag": "=>",
+        "color": "#FFFF00",
+        "strikethrough": false,
+        "underline": false,
+        "backgroundColor": "transparent",
+        "bold": false,
+        "italic": false
+    },
+
+    {
+        "tag": ">",
+        "color": "#CCFF0B",
+        "strikethrough": false,
+        "underline": false,
+        "backgroundColor": "transparent",
+        "bold": false,
+        "italic": false
+    },
+
+    {
+        "tag": "=",
+        "color": "#00FFFF",
+        "strikethrough": false,
+        "underline": false,
+        "backgroundColor": "transparent",
+        "bold": false,
+        "italic": false
+    },
+
+    {
+        "tag": "-",
+        "color": "#4169e1",
+        "strikethrough": false,
+        "underline": false,
+        "backgroundColor": "transparent",
+        "bold": false,
+        "italic": false
+    },
+
+    {
+        "tag": "*",
+        "color": "#9786e0",
+        "strikethrough": false,
+        "underline": false,
+        "backgroundColor": "transparent",
+        "bold": false,
+        "italic": false
+    },
+
+    {
+        "tag": "//",
+        "color": "#474747",
+        "strikethrough": false,
+        "underline": false,
+        "backgroundColor": "transparent",
+        "bold": false,
+        "italic": false
+    }
+
+]
+
+*/
